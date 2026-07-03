216e97ae14c68f57a911bac87aeb2eb5aed0e365
Muhammed Rinshad <mrmuhammedrinshad@gmail.com>
2026-07-03 14:40:08 +0530

    Done

--- Bank.py
+++ Bank.py
@@ -13,3 +13,38 @@ class Bank:
         amt=int(input("Enter amount:"))
         self.bal=self.bal-amt
         print("amount withdrawn")
+    def transfer(self):
+        print("Transferred")
+
+    def balance(self):
+        print("Total Balance:", self.bal)
+
+
+b = Bank()
+ch = 1
+
+while (ch != 0):
+
+    print("1. Add Bank Account")
+    print("2. Deposit")
+    print("3. Withdraw")
+    print("4. Transfer")
+    print("5. Show Balance")
+    print("0. Exit")
+
+    ch = int(input("Enter your choice: "))
+
+    if ch == 1:
+        b.addbank()
+
+    if ch == 2:
+        b.deposit()
+
+    if ch == 3:
+        b.withdraw()
+
+    if ch == 4:
+        b.transfer()
+
+    if ch == 5:
+        b.balance()
