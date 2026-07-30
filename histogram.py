cc624780bdbbe2cd9260749a9a4c5ca815faa070
mr-rinshad <mrmuhammedrinshad@gmail.com>
2026-07-30 14:14:01 +0530

    done

--- histogram.py
+++ histogram.py
@@ -1,16 +1,14 @@
 import plotly.express as px
 
-# Load restaurant tipping data
 df = px.data.tips()
 
-# Create a histogram with marginal rug plots
 fig = px.histogram(
     df,
     x="total_bill",
     color="sex",
     nbins=30,
-    marginal="rug",  # Adds a distribution rug at the bottom
-    title="Distribution of Total Bills by Gender"
+    marginal="rug",
+    title="Distribution of Total Bills by GEnder"
 )
 
 fig.show()
